5de38d7079b3046fe1b365d2fd288a9e9e2cf3df
Rakan <RakanQ5@outlook.com>
2022-03-10 22:08:03 +0300

    socket io client set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { io } from "socket.io-client";
 
 function App() {
+  const socket = io("localhost:5050");
+
+  socket.on("connect", () => {
+    console.log(socket.id);
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
