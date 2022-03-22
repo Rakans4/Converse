128f731abcb4188d4c4a784c18f99a6fac3725b7
Rakan <RakanQ5@outlook.com>
2022-03-22 20:15:23 +0300

    cont layout

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,38 @@ import './App.css';
 import { io } from "socket.io-client";
 import Landingpage from './components/landingPage';
 import Chat from './components/chat';
+import Logo from './components/logo';
 
 function App() {
-  const socket = io("localhost:5050");
+  // useEffect(()=>{
+  //   const socket = io("localhost:5050");
+  //   socket.on("connect", () => {
+  //     console.log(socket.id);
+  //   });
 
-  socket.on("connect", () => {
-    console.log(socket.id);
-  });
+  //   socket.on('code-register', (data)=>{
+  //     console.log(data)
+  //   })
 
+  //   return ()=>{
+  //     //unmount logic here
+  //     console.log('unmount')
+  //   }
+  // }, []);
+
+  // socket.emit('send-message', JSON.stringify())
+
+  function validateChatCode(e){
+    e.preventDefault();
+    console.log('from app.js '+e.target.firstChild.value);
+}
 
   return (
     <div className="App">
-      <Landingpage />
+      <Logo />
+      <Landingpage validateChatCode={validateChatCode} />
       {/* <Chat /> */}
+      
     </div>
   );
 }
--- client/src/components/landingPage.js
+++ client/src/components/landingPage.js
@@ -1,14 +1,15 @@
 import React from "react";
 
-const Landingpage = () => {
+const Landingpage = ({ validateChatCode }) => {
+    
   return (
     <div className="h-screen w-screen flex items-center justify-center">
       <div className=" flex flex-col items-center justify-between h-80 w-[80%]">
-        <div className="text-4xl">CODE01ABC</div>
+        <div className="text-4xl font-mono tracking-wide">CODE01ABC</div>
 
-        <form className="flex flex-col items-center">
+        <form onSubmit={validateChatCode} className="flex flex-col items-center">
           <input type="text" placeholder="enter a code..." className="h-12 w-max px-6 border-[0.5px] border-slate-300 rounded-3xl" />
-          <button className="w-[70%] h-12 bg-yellow-200 rounded-3xl mt-6 hover:bg-yellow-400">Start a chat</button>
+          <button type="submit" className="w-[70%] h-12 bg-yellow-200 rounded-3xl mt-6 hover:bg-yellow-400">Start a chat</button>
         </form>
       </div>
     </div>
